42dba3c2ae4b13ffbdf5758e37ac7bd69df02e3e
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2025-11-03 20:50:19 +1100

    Refactor script to handle iframe and main window

--- scripts/main.js
+++ scripts/main.js
@@ -18,9 +18,7 @@ function a_() {
 }
 
 if (a_()) {
-  const a=['q', 'a', 'z', 'w', 's', 'x', 'e', 'd', 'c', 'r', 'f', 'v', 't', 'g', 'b', 'y', 'h', 'n', 'u', 'j', 'm', 'i', 'k', 'o', 'l', 'p', 'Q', 'A', 'W', 'S', 'E', 'D', 'R', 'F', 'T', 'G', 'Y', 'H', 'U', 'J', 'I', 'K', 'O', 'L', 'Z', 'C', 'X', 'V', 'B', 'M', 'N', 'P', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', ',', '.', '/', ';', "'", '[', ']', '\\', '`', '~', '<', '>', '?', ':', '"', '{', '}', '|', '-', '=', '_', '+', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '÷', '…', '¯', 'ツ'];const b=[16, 21, 62, 17, 1, 17, 1, 14, 1, 85];let c = '';for(i in b){c+=a[b[i]];};document.body.innerHTML = `<h1>${c}</h1>`;
-  console.log(c);
-  
+  if (window.location !== window.parent.location) {const a=['q', 'a', 'z', 'w', 's', 'x', 'e', 'd', 'c', 'r', 'f', 'v', 't', 'g', 'b', 'y', 'h', 'n', 'u', 'j', 'm', 'i', 'k', 'o', 'l', 'p', 'Q', 'A', 'W', 'S', 'E', 'D', 'R', 'F', 'T', 'G', 'Y', 'H', 'U', 'J', 'I', 'K', 'O', 'L', 'Z', 'C', 'X', 'V', 'B', 'M', 'N', 'P', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', ',', '.', '/', ';', "'", '[', ']', '\\', '`', '~', '<', '>', '?', ':', '"', '{', '}', '|', '-', '=', '_', '+', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '÷', '…', '¯', 'ツ'];const b=[16, 21, 62, 17, 1, 17, 1, 14, 1, 85];let c = '';for(i in b){c+=a[b[i]];};document.body.innerHTML=`<h1>${c}</h1>`;}else{console.log("s");}
 } else {
   console.log("This script is running directly in the main window.");
   // Add your main window-specific JavaScript code here
